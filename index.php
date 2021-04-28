a1202f8cfcf5330b2e7354dd5535d148ed2c42d8
jessica3072 <78502298+jessica3072@users.noreply.github.com>
2021-04-28 15:06:41 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    <meta charset="UTF-8">
+    </head> 
+<body>
+Welcome to my web site! 
+    
+    Click <a href="allitems.php">here</a> to view all items. 
+
+
+
+<?php
+session_start();
+
+
+include_once 'includes/header.php';
+
+
+
+/*$query = "SELECT pet_id, name, age, sex, species, caretaker_name, email 
+		FROM pets NATURAL JOIN caretakers ORDER BY pet_id ASC";
+$result = $conn->query($query);
+if (!$result) die ("Database access failed: " . $conn->error);
+$rows = $result->num_rows;
+
+echo "<table><tr> <th>Id</th> <th>Name</th><th>Age</th><th>Sex</th><th>Species</th><th>Caretaker</th><th>Email</th></tr>";
+while ($row = $result->fetch_assoc()) {
+	echo '<tr>';
+	echo "<td>".$row["pet_id"]."</td><td>";
+	echo "<a href=\"viewpet.php?id=".$row["pet_id"]."\">".$row["name"]."</a>";
+	echo "</td><td>".$row["age"]."</td><td>".$row["sex"]."</td><td>".$row["species"]."</td><td>".$row["caretaker_name"]."</td>";		
+	echo "<td><a href=\"mailto:".$row["email"]."\">".$row["email"]."</a></td>";
+	echo '</tr>';
+}
+
+echo "</table>";
+echo "<a href=\"addnewpet.php\">Add a New Pet</a>";
+
+*/
+
+include_once 'includes/footer.php';
+?>
---
+++ viewitem.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    <meta charset="UTF-8">
+    </head> 
+<body>
+Hellow world
+    
+    Click <a href="index.php">here</a>a> to return to homepage. 
+    
+    
+    <?php 
+
+
+?>
+
+</body>
+</html>
