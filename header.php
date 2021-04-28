407629f60e4ac39241821b14e0b481f1c790c4ea
jessica3072 <78502298+jessica3072@users.noreply.github.com>
2021-04-28 15:07:55 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Item Database</title>
+/*<link rel="stylesheet" type="text/css" href="includes/inventory.css" />
+    */
+</head>
+<body>
+<header>
+	<a href="index.php">
+        
+<img src="images/shelter_logo.jpg" width="70" height="70"></a>
+	<h1>Inventory Database</h1>
+	<?php
+
+		echo " | <small>Thank you</small></h3>";
+	}
+	?>
+</header>
+<div id="body">
